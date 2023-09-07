a2c330dacc6dd811e6a2853df02926af0c74733b
guisant <guilhermepacheco253@gmail.com>
2023-09-06 21:23:39 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Container from '@mui/material'
+import ButtonAppBar from './components/ButtonAppBar'
+
+function App() {
+  return (
+    <>
+    <Container>
+        <ButtonAppBar />
+    </Container>
+    </>
+  )
+}
+
+export default App
