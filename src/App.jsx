8ae81753c9703bf5029858632783b5dd84484feb
guisant <guilhermepacheco253@gmail.com>
2023-09-08 19:06:26 -0300

    feat: import NavAppBar in index

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
-import ButtonAppBar from './components/ButtonAppBar'
+import Container from '@mui/material/Container'
+import NavAppBar from './components/NavAppBar'
+import BannerHome from './components/BannerHome'
 
 function App() {
   return (
     <>
     <div>
-        <ButtonAppBar />
+      <Container>
+        <NavAppBar />
+        <Container>
+          <BannerHome />
+        </Container>
+      </Container>
     </div>
     </>
   )
