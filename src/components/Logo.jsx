ec3dc105eb4bea6e9595d5a73adcb54df8c9e599
guisant <guilhermepacheco253@gmail.com>
2023-09-08 19:08:03 -0300

    feat: change logo size and adjust padding

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,10 +1,15 @@
 import Box from '@mui/material/Box';
 import gsLogo from '../assets/logo.svg'
 
+const logoStyle = {
+    width: 170,
+}
+
+
 export default function Logo() {
     return (
-        <Box>
-            <img src={gsLogo} className="logo" alt="GS Oficial logo" />
+        <Box sx={{ pl: { xs: 'none', sm: 7 } }}>
+            <img src={gsLogo} style={logoStyle} alt="GS Oficial logo" />
         </Box>
     )
 }
