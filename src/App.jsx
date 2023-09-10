cdd9c85731554a6e1b8e7e08048e1d648cff8e07
guisant <guilhermepacheco253@gmail.com>
2023-09-10 15:24:04 -0300

    feat: add material ui theme and import to index

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,21 @@
 import Container from '@mui/material/Container'
 import NavAppBar from './components/NavAppBar'
 import BannerHome from './components/BannerHome'
+import { ThemeProvider } from '@mui/material/styles'
+import theme from './styles/theme'
 
 function App() {
   return (
     <>
     <div>
-      <Container>
-        <NavAppBar />
-        <Container>
-          <BannerHome />
-        </Container>
+    <ThemeProvider theme={theme}>
+      <Container maxWidth='xl'>
+        <header>
+          <NavAppBar />
+        </header>
+            <BannerHome />
       </Container>
+    </ThemeProvider>
     </div>
     </>
   )
---
+++ src/styles/theme.jsx
@@ -0,0 +1,12 @@
+import { createTheme } from '@mui/material/styles';
+
+const theme = createTheme({
+    typography: {
+        button: {
+          fontFamily: "'Rubik', sans-serif",
+          fontWeight: 400,
+        },
+      },
+});
+
+export default theme
