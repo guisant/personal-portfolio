7c14ddac438c0c230189a6993f27fd7ab419cb73
guisant <guilhermepacheco253@gmail.com>
2023-09-11 15:04:07 -0300

    feat: add ButtonsAboutMe Component

---
+++ src/components/ButtonsAboutMe.jsx
@@ -0,0 +1,11 @@
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
+
+export default function ButtonsAboutMe() {
+  return (
+    <Stack spacing={3} direction="row">
+      <Button variant="outlined" size="large" sx={{ color: '#fff' }} color='secondary'>Currículo</Button> 
+      <Button variant="outlined" size="large" sx={{ ml: 5, color: '#fff' }} color='secondary'>Entrar em contato</Button>
+    </Stack>
+  );
+}
