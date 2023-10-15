b99014a1f9c3504eb3553dfcd3df432d7b004f4a
guisant <guilhermepacheco253@gmail.com>
2023-10-15 16:32:06 -0300

    :sparkles: feat: add function to download cv pdf

--- src/components/ButtonsAboutMe.jsx
+++ src/components/ButtonsAboutMe.jsx
@@ -1,11 +1,34 @@
 import Stack from '@mui/material/Stack';
 import Button from '@mui/material/Button';
+import CloudDownloadIcon from '@mui/icons-material/CloudDownload';
+import MailOutlineIcon from '@mui/icons-material/MailOutline';
 
-export default function ButtonsAboutMe() {
+const ButtonsAboutMe = () => {
+
+  const onButtonClick = () => {
+     
+    // using Java Script method to get PDF file
+    fetch("cv-guilherme.pdf").then((response) => {
+        response.blob().then((blob) => {
+         
+            // Creating new object of PDF file
+            const fileURL =
+                window.URL.createObjectURL(blob);
+                 
+            // Setting various property values
+            let alink = document.createElement("a");
+            alink.href = fileURL;
+            alink.download = "cv-guilherme.pdf";
+            alink.click();
+        });
+    });
+  };
+
   return (
     <Stack spacing={3} direction="row">
-      <Button variant="outlined" size="large" sx={{ color: '#fff' }} color='secondary'>Currículo</Button> 
-      <Button variant="outlined" size="large" sx={{ ml: 5, color: '#fff' }} color='secondary'>Fale comigo!</Button>
+      <Button variant="outlined" size="large" sx={{ color: '#fff' }} color='secondary' startIcon={<CloudDownloadIcon />} onClick={onButtonClick}>Currículo</Button> 
+      <Button variant="outlined" size="large" sx={{ ml: 5, color: '#fff' }} color='secondary' startIcon={<MailOutlineIcon />}>Contato</Button>
     </Stack>
   );
 }
+export default ButtonsAboutMe;
