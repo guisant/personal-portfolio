e740f9f674efee30577b77e83100d395e764c985
guisant <guilhermepacheco253@gmail.com>
2023-10-15 16:33:23 -0300

    :sparkles: feat: add react icons dependency

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -4,8 +4,17 @@ import {
     CardMedia,
     Typography
 } from '@mui/material';
+import Stack from '@mui/material/Stack';
 import ButtonsAboutMe from './ButtonsAboutMe'
 import profilePic from '../assets/profile_pic.jpg'
+import { 
+    BiLogoReact,
+    BiLogoTypescript,
+    BiLogoNodejs,
+    BiLogoPython
+} from 'react-icons/bi';
+import { SiMysql } from "react-icons/si";
+import { RiJavascriptFill } from "react-icons/ri";
 
 export default function AboutMe() {
     return (
@@ -17,7 +26,7 @@ export default function AboutMe() {
                 pt: { xs: 7, sm: 3 },
                 pb: { xs: 7, sm: 10 }
             }}>
-                <Box sx={{ ml: 13, mt: 3, display: { xs: 'none', sm: 'flex' } }}> 
+                <Box sx={{ ml: 17, mt: 3, pt: 4, display: { xs: 'none', sm: 'flex' } }}> 
                     <Card sx={{ width: 250 }}>
                         <CardMedia
                             sx={{ height: 250, width: 250 }}
@@ -35,17 +44,24 @@ export default function AboutMe() {
                         <Typography variant='h5' sx={{ pb: { xs: 1, sm: 2 } }}>
                             São Paulo, Brasil
                         </Typography>
-                        <Typography color={'#C4C4C4'}>
+                        <Typography color={'#C4C4C4'} sx={{ pr: { sm: 7 } }}>
                             Lorem ipsum risus eleifend phasellus ultricies nibh inceptos imperdiet, 
                             semper blandit aenean aliquam lectus bibendum molestie massa, rutrum porttitor 
                             ullamcorper vitae ultrices eleifend nibh. 
                         </Typography>
                     </Box>  
-                    <Box sx={{ pt: { xs: 4, sm: 4 } }}>
-                        icons skills
+                    <Box sx={{ pt: 4 }}>
+                        <Stack spacing={2} direction="row">
+                            <BiLogoReact size={28} color='#088a5f'/>
+                            <RiJavascriptFill size={28} color='#088a5f'/>
+                            <BiLogoTypescript size={28} color='#088a5f'/>
+                            <BiLogoNodejs size={28} color='#088a5f'/>
+                            <BiLogoPython size={28} color='#088a5f'/>
+                            <SiMysql size={30} color='#088a5f'/>
+                        </Stack>
                     </Box>
                     <Box sx={{ 
-                        pt: { xs: 4, sm: 4 }, 
+                        mt: 4, 
                         display: 'flex', 
                         justifyContent: { xs: 'center', sm: 'start' } 
                     }}>
